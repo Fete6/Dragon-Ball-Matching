f6cd919a92d1b7afa6cd46803a5b7383c31084b0
Casper <scumworks@gmail.com>
2018-03-08 17:07:54 +0100

    Added Database. Deleted cache file and added a .gitignore to avoid "junk" in commit.

---
+++ DragonBallMatching/Properties/Database.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonBallMatching {
+    public static class Database {
+        public static string DataSource = "(local)";
+        //        public static string DataSource = ".\\SQLEXPRESS";
+
+        public static void SQLkommandoSet(string sqltext) {
+            string ConnectionString = $@"Data Source = {DataSource}; Initial Catalog = Dragon_Ball_Matching; Integrated Security = True";
+            var connection = new SqlConnection(ConnectionString);
+            SqlCommand cmd;
+            connection.Open();
+
+            try {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = sqltext;
+                cmd.ExecuteNonQuery();
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                throw;
+            } finally {
+                if (connection.State == System.Data.ConnectionState.Open) {
+                    connection.Close();
+                }
+            }
+        }
+
+        public static List<string> SQLkommandoGet(string sqltext) {
+            string ConnectionString = $@"Data Source = {DataSource}; Initial Catalog = Dragon_Ball_Matching; Integrated Security = True";
+            var connection = new SqlConnection(ConnectionString);
+            List<string> SQLList = new List<string>();
+
+            connection.Open();
+            using (SqlCommand command = new SqlCommand(sqltext, connection)) {
+                using (SqlDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            SQLList.Add(reader.GetValue(i).ToString());
+                        }
+                    }
+                }
+            }
+            connection.Close();
+            return SQLList;
+        }
+    }
+}
